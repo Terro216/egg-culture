e6f868f7461b9588ed1f95d5212e995e473abf3a
ilyamed <ilyamed+github@yandex.ru>
2026-04-28 19:49:46 +0300

    some

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import { loadEnv } from "vite";
+
+const env = loadEnv(process.env.NODE_ENV || "development", process.cwd(), "");
+const domain = env.DOMAIN || "ilyamedve.dev";
 
 // https://astro.build/config
 export default defineConfig({
-  site: `https://egg.ilyamedve.dev/`,
+  site: `https://egg.${domain}/`,
   integrations: [react()],
   i18n: {
     locales: ["ru", "en"],
